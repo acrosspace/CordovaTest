5f6cef79f267fb03867bb5feef1999d932ffd782
Cass Woo <casswoo@gmail.com>
2018-11-29 08:00:03 +0900

    Add project files.

---
+++ CordovaApp1/www/cordova.js
@@ -0,0 +1,60 @@
+﻿// Visual Studio Ripple Error DEP10401
+// https://stackoverflow.com/questions/34096973/visual-studio-ripple-error-dep10401
+// https://stackoverflow.com/questions/34096973/visual-studio-ripple-error-dep10401
+//Run "regedit"
+//Go to path "HKEY_LOCAL_MACHINE\SOFTWARE\Clients\StartMenuInternet\Google Chrome"
+//Change default value "Chrome" to "Google Chrome"
+//Try again build.
+
+
+
+// https://docs.microsoft.com/en-us/windows/uwp/get-started/get-started-tutorial-game-js3d
+
+// https://cordova.apache.org/
+// https://docs.microsoft.com/en-us/visualstudio/cross-platform/tools-for-cordova/?view=toolsforcordova-2017
+// https://github.com/Microsoft/cordova-samples
+// https://github.com/Microsoft/cordova-samples/tree/master/weather-app
+// https://docs.microsoft.com/en-us/visualstudio/cross-platform/tools-for-cordova/first-steps/get-started-with-ionic2?view=toolsforcordova-2017
+// https://visualstudio.microsoft.com/vs/features/cordova/
+
+// Using merges to Customize Each Platform
+// https://cordova.apache.org/docs/en/latest/guide/cli/#using-merges-to-customize-each-platform
+
+// Customize Icons
+// https://cordova.apache.org/docs/en/latest/config_ref/images.html
+
+// cordova-plugin-splashscreen
+// https://cordova.apache.org/docs/en/latest/reference/cordova-plugin-splashscreen/index.html
+
+
+// https://cordova.apache.org/docs/en/latest/guide/overview/index.html
+// https://cordova.apache.org/docs/en/latest/guide/hybrid/webviews/index.html
+// https://cordova.apache.org/plugins/?q=bluetooth
+
+// https://cordova.apache.org/docs/en/latest/guide/hybrid/plugins/index.html
+
+
+
+
+
+
+
+
+// Bower, A package manager for the web
+// https://bower.io/
+
+// Add packages to your Cordova project with the Bower Package Manager
+// https://docs.microsoft.com/en-us/visualstudio/cross-platform/tools-for-cordova/first-steps/using-bower?view=toolsforcordova-2017
+
+// Package Your Cordova App for Publishing to an App Store
+// https://docs.microsoft.com/en-us/visualstudio/cross-platform/tools-for-cordova/publishing/publish-to-a-store?view=toolsforcordova-2017
+
+
+// 컴퓨터\HKEY_LOCAL_MACHINE\SOFTWARE\Clients\StartMenuInternet\Google Chrome
+// Google Chrome
+
+// check http://jquerymobile.com/download/ to make sure the compatibility
+
+
+
+
